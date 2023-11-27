0b9bc84b5a5e418e4dc20f54e0842f5fcfd387e6
samipevekar <114756162+samipevekar@users.noreply.github.com>
2023-11-27 19:00:53 +0530

    script.js

--- script.js
+++ script.js
@@ -2,12 +2,21 @@ let submit=document.getElementById("sub")
 let output=document.getElementById("output")
 submit.addEventListener("click",()=>{
     
-let input=document.getElementById('weather').value
+let input=document.getElementById('weather').value.trim()
     let api ="https://goweather.herokuapp.com/weather/"+input
     fetch(api)
 .then(res=>{
+    if(res.ok){
+        
     return res.json()
+
+    }
+    else{
+        document.getElementById('output').insertAdjacentHTML("beforeend",`<h2>Error 404 : try after some time</h2>`)
+    }
 }).then(text =>{
+    
+    
     document.getElementById('output').innerHTML = '';
     document.getElementById('output').insertAdjacentHTML("beforeend",`<p>Temperature : ${text.temperature}</p>`)
     document.getElementById('output').insertAdjacentHTML("beforeend",`<p>Wind : ${text.wind}</p>`)
@@ -21,16 +30,14 @@ let input=document.getElementById('weather').value
     else if(text===''){
         document.getElementById("output").innerHTML="Error : application is not working"
     }
-
+    else if(text.input===''){
+        document.getElementById("output").innerHTML="Enter your city name"
+    }
     
     else{
         output.style.color="black"
     }
     console.log(text)
-}).catch(err=>{
-    document.getElementById("output").innerHTML=""
-    
-    document.getElementById('output').insertAdjacentHTML("beforeend",`<span>Enter your city name</span>`)
 })
 
 
