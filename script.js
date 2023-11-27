c220b8bf16e5fae78ac475e78d5f74b902706e9b
samipevekar <114756162+samipevekar@users.noreply.github.com>
2023-11-27 12:42:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let submit=document.getElementById("sub")
+let output=document.getElementById("output")
+submit.addEventListener("click",()=>{
+    
+let input=document.getElementById('weather').value
+    let api ="https://goweather.herokuapp.com/weather/"+input
+    fetch(api)
+.then(res=>{
+    return res.json()
+}).then(text =>{
+    document.getElementById('output').innerHTML = '';
+    document.getElementById('output').insertAdjacentHTML("beforeend",`<p>Temperature : ${text.temperature}</p>`)
+    document.getElementById('output').insertAdjacentHTML("beforeend",`<p>Wind : ${text.wind}</p>`)
+    document.getElementById('output').insertAdjacentHTML("beforeend",`<p>Weather : ${text.description}</p>`)
+
+    if(text.temperature=='' || text.wind=='' || text.description=='' ){
+        document.getElementById("output").innerHTML="Error : enter your city name"
+        output.style.color="red"
+        output.style.fontSize="20px"
+    }
+    else if(text===''){
+        document.getElementById("output").innerHTML="Error : application is not working"
+    }
+
+    
+    else{
+        output.style.color="black"
+    }
+    console.log(text)
+})
+
+})
+
